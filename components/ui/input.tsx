ad6ceedbc76cccc6278765a05ca9312dbfaaa31f
Sally Kim <sallyk0127@gmail.com>
2025-03-07 15:57:48 +1100

    Fix ESLint empty object type issue

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 export const Input: React.FC<InputProps> = (props) => (
   <input {...props} className="w-full p-2 border rounded-md" />
--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {}
+type SelectProps = React.SelectHTMLAttributes<HTMLSelectElement>;
 
 export const Select: React.FC<SelectProps> = ({ children, ...props }) => {
   return (
