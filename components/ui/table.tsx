3dee319ab082e7fafd551f5df054472039841b75
Sally Kim <sallyk0127@gmail.com>
2025-03-11 16:08:36 +1100

    fixed error in ui/table.tsx

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -13,8 +13,8 @@ export const TableBody = ({ children }: { children: React.ReactNode }) => {
 }
 
 export const TableRow = ({ children, className }: { children: React.ReactNode; className?: string }) => {
-  return <tr className={'border-b border-gray-300 ${className || ""}'}>{children}</tr>
-}
+  return <tr className={`border-b border-gray-300 ${className ?? ""}`}>{children}</tr>;
+};
 
 export const TableCell = ({ children }: { children: React.ReactNode }) => {
   return <td className="p-2 border border-gray-300">{children}</td>
